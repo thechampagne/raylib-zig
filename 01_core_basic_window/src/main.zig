2072fa937d1e8cdf54fe3b8342812e6162e403f5
XXIV <13811862+thechampagne@users.noreply.github.com>
2022-07-22 22:00:08 +0300

    Add core_basic_window example

---
+++ 01_core_basic_window/src/main.zig
@@ -0,0 +1,42 @@
+const c = @cImport(@cInclude("raylib.h"));
+
+const screenWidth = 800;
+const screenHeight = 450;
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+pub fn main() void {
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    c.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    c.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!c.WindowShouldClose()) { // Detect window close button or ESC key
+
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        c.BeginDrawing();
+
+        c.ClearBackground(c.RAYWHITE);
+
+        c.DrawText("Congrats! You created your first window!", 190, 200, 20, c.LIGHTGRAY);
+
+        c.EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //-------------------------------------------------------------------------------------
+    c.CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+}
